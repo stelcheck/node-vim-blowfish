2caa2040481518bb3bdb64ec91b32bece99be134
Mark Fassler <mfassler@wizcorp.jp>
2013-03-08 16:22:59 +0900

    WIP:  decrypts the first block (64 bytes) of a blowfish-encrypted vim file  also:  added a python version, with the same limitations

--- test.js
+++ test.js
@@ -5,94 +5,93 @@ var fs      = require('fs');
 var data    = fs.readFileSync('file');
 var pass    = 'test';
 
-var isEncrypted = data.slice(0, 12).toString() === 'VimCrypt~02!';
+var header = data.slice(0, 12);
+var isEncrypted = header.toString() === 'VimCrypt~02!';
+
+
+function getKey(password, salt) {
+    var hash = crypto.createHash('sha256');
+
+	hash.update(password);
+	hash.update(salt);
+
+	var key = hash.digest('hex');
+
+    /* Process the key 1000 times.
+        See http://en.wikipedia.org/wiki/Key_strengthening */
+
+	for (var i=0; i<1000; i++) {
+		hash = crypto.createHash('sha256');
+		hash.update(key);
+		hash.update(salt);
+		key = hash.digest('hex');
+	}
+
+	return key;
+}
+
+
+function flipEndian(inData) {
+	var bLen = inData.length;
+	var outData = new Buffer(bLen);
+
+	for (var i=0; i<bLen; i+=4) {
+        outData.writeUInt32BE(inData.readUInt32LE(i), i);
+	}
+	return outData;
+}
+
+
+function repeatBuffer(inBuffer, n) {
+	var inLen = inBuffer.length;
+	var outLen = inLen * n;
+	out = new Buffer(outLen);
+
+	for (var i = 0; i<n; i++) {
+		for (var j=0; j<inLen; j++) {
+			out[ i * inLen + j] = inBuffer[j];
+		}
+	}
+	return out;
+}
+
 
 if (isEncrypted) {
-    /**
-     * We remove the file encryption type identifier, then extract the salt and
-     * the IVR
-     */
-    data = data.slice(12);
+    var salt = data.slice(12,20);
+    var iv   = data.slice(20,28);
+	var contents = data.slice(28);
 
-    var salt = data.slice(0,8);
-    var ivr  = data.slice(8,16);
-
-    data = data.slice(16);
-    pass = new Buffer(pass, 'ascii');
-
-    console.log('salt ::', salt.length, salt.toString('hex'));
-    console.log('ivr  ::', ivr.length, ivr.toString('hex'));
-    console.log('pass ::', pass.length, pass.toString('hex'));
-
-    /**
-     * Hashing
-     */
-    // console.log('\n\nhashing');
-
-    key = sha256_hex(pass, salt);
-
-    for (var i = 0; i < 1000; i++){
-        /* if (i < 5) {
-            console.log('key  ::', key.length, key.toString('hex'), "(salt: ", salt.toString('hex'), ")");
-        } */
-
-        key = sha256_hex(key, salt);
-    }
-
-    console.log('\nafter hashing');
-    console.log('key  ::', key.length, key.toString('hex'));
+    var key = getKey(pass, salt);
+	console.log(' key: ', key);
+	var binKey = new Buffer(key, 'hex');
 
     /**
      * Decryption process
      */
-    var bf      = crypto.createCipheriv('bf-cfb', key, ivr);
+    var bf = crypto.createCipheriv('bf-ecb', binKey, '');
 
-    console.log('\nData to decrypt', data.length);
 
-    console.log(data);
+	//   ****************
+	// TODO:  FIXME:  This only decrypts the first block
+	//   ****************
 
-    var store = new Buffer(8), output = new Buffer(0);
+	// Initialize the keystream:
+	iv_be = repeatBuffer(flipEndian(iv), 8);
+	var keystream_be = new Buffer(bf.update(iv_be, 'binary'), 'binary');
+	var keystream = flipEndian(keystream_be);
 
-    console.log('\nDecrypting blocks');
+    console.log('\n    ****  Decrypting ONLY the first block:');
 
-    for (var j = 0; j < data.length; j += 8) {
+	var blockLen = 64;
+	blockLen = Math.min(blockLen, keystream.length);
+	blockLen = Math.min(blockLen, contents.length);
 
-        // Change from little little-endian to big-endian
-        store.writeUInt32BE(data.readUInt32LE(j), 0);
-        store.writeUInt32BE(data.readUInt32LE(j + 4), 4);
+	var output = new Buffer(blockLen);
+    for (var i=0; i<blockLen; i++) {
+		output[i] = keystream[i] ^ contents[i];
+	}
 
-        console.log('');
-        console.log('convert\t:', data.slice(j, j+8).toString('hex') + '\t', store.length + ':' + store.toString('hex') + '\t');
-
-        // Crypt/decrypt
-        res = new Buffer(bf.update(store), 'binary');
-        console.log('decrypt\t:', store.toString('hex') + '\t', res.length + ':' + res.toString('hex') + "\t");
-
-        // Change from little little-endian to big-endian
-        store.writeUInt32BE(res.readUInt32LE(0), 0);
-        store.writeUInt32BE(res.readUInt32LE(4), 4);
-        console.log('res\t:', data.slice(j, j+8).toString('hex') + '\t', store.length + ':' + store.toString('hex') + '\t');
-
-        output = Buffer.concat([output, store]);
-    }
-
-    output = Buffer.concat([output, new Buffer(bf.final('binary'))]);
+	console.log(output.toString());
 }
 
-console.log('');
-console.log(data.length + ' bytes crypted\t', data, data.toString());
-console.log(output.length + ' bytes decrypted\t', output, output.toString());
 
-var orig = fs.readFileSync('file.decrypted');
-console.log(orig.length + ' bytes original\t', orig, orig.toString());
-
-function sha256_hex() {
-    var hash = crypto.createHash('sha256');
-    var buffers = Array.prototype.slice.call(arguments, 0);
-
-    for (var i = 0; i < buffers.length; i++) {
-        hash.update(buffers[i]);
-    }
-
-    return hash.digest('hex');
-}
